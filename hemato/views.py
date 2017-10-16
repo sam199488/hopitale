738206bc29a2145fe7e77d4633e28ff421e18802
sam199488 <ssdev1994@gmail.com>
2017-10-16 16:22:15 +0200

    mysql conf

--- hemato/views.py
+++ hemato/views.py
@@ -17,7 +17,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def index(request):
-    return render(request, 'hemato/accueil.html')
+    #return render(request, 'hemato/accueil.html')
+    return render(request, 'hemato/b.html')
 
 class Login(TemplateView):
 
