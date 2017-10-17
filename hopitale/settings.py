04bfab07285568096695bb79c3729f3e8a8c324f
sam199488 <ssdev1994@gmail.com>
2017-10-17 23:39:23 +0200

    mysql conf

--- hopitale/settings.py
+++ hopitale/settings.py
@@ -139,12 +139,14 @@ LOGIN_REDIRECT_URL = '/hemato/'
 #    # Add to this list all the locations containing your static files
 #)
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = 'staticfiles'
-
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 #STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    (os.path.join(BASE_DIR,'hemato','static')),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
-
